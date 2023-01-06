28f693949461de00e36a39cb4a80db30b69c32a4
AugusDogus <9794679+AugusDogus@users.noreply.github.com>
2023-01-05 20:38:30 -0600

    Reproduce Issue

--- src/pages/_app.tsx
+++ src/pages/_app.tsx
@@ -1,9 +1,20 @@
 import { type AppType } from "next/dist/shared/lib/utils";
-
+import localFont from '@next/font/local';
 import "../styles/globals.css";
 
+const wingdings = localFont({
+  src: [
+    {
+      path: "./fonts/wingdings.woff2",
+      weight: '400',
+      style: 'normal',
+    }
+  ],
+  variable: '--font-wingdings',
+});
+
 const MyApp: AppType = ({ Component, pageProps }) => {
-  return <Component {...pageProps} />;
+  return <div className={`${wingdings.variable} font-mono h-screen`}><Component {...pageProps} /></div>;
 };
 
 export default MyApp;
